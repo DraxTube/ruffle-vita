3bdeb41dcb4911c054da024ecd4624c504b06652
DraxTube <draxtube01@gmail.com>
2026-01-14 21:05:49 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,15 @@
 // Importiamo i componenti della PS Vita
 use psp2_sys as libvita;
 
-#[no_mangle]
-pub extern "C" fn main(_argc: i32, _argv: *mut *mut u8) -> i32 {
-    // 1. Inizializzazione Hardware della Vita (Schermo e Audio)
-    // Qui andranno i comandi per accendere il display
-    
-    // 2. Caricamento del motore Ruffle
-    // let player = ruffle_core::Player::new(...);
-
-    // 3. Ciclo infinito per far girare il gioco
-    loop {
-        // Leggi input (tasti Vita)
-        // Disegna frame Flash sullo schermo GXM
-    }
-}
-
+// Questo pezzo serve a gestire gli errori se il programma crasha
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn main(_argc: i32, _argv: *mut *mut u8) -> i32 {
+    // Per ora il programma non fa nulla, vogliamo solo vedere se compila
+    // il "cuore" di Ruffle insieme al codice Vita.
+    0
 }
