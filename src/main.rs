72ee4856cb5534549a922828b6e5fd88c6c97a41
DraxTube <draxtube01@gmail.com>
2026-01-14 21:09:48 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 #![no_std]
 #![no_main]
 
-// Importiamo i componenti della PS Vita
-use psp2_sys as libvita;
-
-// Questo pezzo serve a gestire gli errori se il programma crasha
-#[panic_handler]
-fn panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
+// Usiamo la nuova libreria vitasdk
+use vitasdk_sys as libvita;
 
 #[no_mangle]
 pub extern "C" fn main(_argc: i32, _argv: *mut *mut u8) -> i32 {
-    // Per ora il programma non fa nulla, vogliamo solo vedere se compila
-    // il "cuore" di Ruffle insieme al codice Vita.
     0
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
